0e4d5e501b97fd48984aa445e0b8de8bfa4ebdd6
Anastasios Statiris <tasosstatiris@outlook.fr>
2026-01-08 22:27:45 +0100

    search UI to test daisyUI and tailwind and define the App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
-import './App.css';
+import "./App.css";
+import Search from "./components/Search";
 
 function App() {
-  return <>
-  </>;
+  return (
+    <main className="min-h-screen flex flex-col items-center">
+      <header className="text-5xl font-bold my-6"><h1>Movie App</h1></header>
+
+
+      <Search />
+
+      <section></section>
+      <section></section>
+      <section></section>
+
+      <footer></footer>
+    </main>
+  );
 }
 
 export default App;
