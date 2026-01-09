7d8cdb1cee83abe13b3e9e7e6e38e1bb5b0bcd09
Anastasios Statiris <tasosstatiris@outlook.fr>
2026-01-09 08:56:46 +0100

    3d hover effect for cards

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -7,20 +7,29 @@ const MovieList = ({ movies }) => {
           : "https://via.placeholder.com/500x750?text=No+Image";
 
         return (
-          <div
-            key={movie.id}
-            className="card bg-base-100 image-full w-64 shadow-sm hover:shadow-xl transition-shadow cursor-pointer"
-          >
-            <figure>
-              <img src={imageUrl} alt={movie.title} />
-            </figure>
-            <div className="card-body">
-              <h2 className="card-title">{movie.title}</h2>
-              <p>⭐ {movie.vote_average.toFixed(1)}</p>
-              <div className="card-actions justify-end">
-                <button className="btn btn-soft">Voir les détails</button>
+          <div key={movie.id} className="hover-3d cursor-pointer">
+            {/* content */}
+            <div className="card bg-base-100 image-full w-64 shadow-sm hover:shadow-xl transition-shadow">
+              <figure>
+                <img src={imageUrl} alt={movie.title} />
+              </figure>
+              <div className="card-body">
+                <h2 className="card-title">{movie.title}</h2>
+                <p>⭐ {movie.vote_average.toFixed(1)}</p>
+                <div className="card-actions justify-end">
+                  <button className="btn btn-soft">Voir les détails</button>
+                </div>
               </div>
             </div>
+            {/* 8 empty divs needed for the 3D effect */}
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
           </div>
         );
       })}
